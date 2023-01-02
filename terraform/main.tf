f080f45615491dabfbd47472b6493de4d3e0edbf
Amine Kherbouche <kaminek92@gmail.com>
2023-01-02 23:50:31 +0100

    feat: add tf infra
    Signed-off-by: Amine Kherbouche <kaminek92@gmail.com>

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+
+locals {
+  fleet_count = 3
+  flavour     = "2xCPU-4GB"
+}
+
+resource "upcloud_server" "worker" {
+  count    = local.fleet_count
+  plan     = local.flavour
+  hostname = "worker${count.index}"
+  zone     = "uk-lon1"
+
+  template {
+    storage = "Ubuntu Server 22.04 LTS (Jammy Jellyfish)"
+  }
+
+  network_interface {
+    type = "public"
+  }
+
+  labels = {
+    cluster = "homelab"
+    env     = "prod"
+    node    = "worker${count.index}"
+  }
+
+  login {
+    keys = data.github_user.kaminek.ssh_keys
+  }
+}
