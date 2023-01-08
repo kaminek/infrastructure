ff7087530c4b2d4c08dfd3cf92f83c98083a8f60
Amine Kherbouche <kaminek92@gmail.com>
2023-01-08 23:44:09 +0100

    feat(tf): add private subnet
    Signed-off-by: Amine Kherbouche <kaminek92@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,13 +2,14 @@
 locals {
   fleet_count = 3
   flavour     = "2xCPU-4GB"
+  region      = "uk-lon1"
 }
 
 resource "upcloud_server" "worker" {
   count    = local.fleet_count
   plan     = local.flavour
   hostname = "node${count.index}"
-  zone     = "uk-lon1"
+  zone     = local.region
 
   template {
     storage = "01000000-0000-4000-8000-000030220200"
@@ -17,6 +18,15 @@ resource "upcloud_server" "worker" {
 
   network_interface {
     type = "public"
+  }
+
+  network_interface {
+    type                = "private"
+    bootable            = false
+    source_ip_filtering = false
+    ip_address_family   = "IPv4"
+    network             = upcloud_network.k8s_vpc.id
+    ip_address          = "10.8.0.${count.index + 1}"
   }
 
   labels = {
@@ -30,4 +40,16 @@ resource "upcloud_server" "worker" {
   }
 
   metadata = true
+}
+
+resource "upcloud_network" "k8s_vpc" {
+  name = "k8s-vpc"
+  zone = local.region
+
+  ip_network {
+    address            = "10.8.0.0/24"
+    dhcp               = false
+    dhcp_default_route = false
+    family             = "IPv4"
+  }
 }
