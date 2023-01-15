0eb29587d4b352e9467104fc9a1f426b9bbeeb6d
Amine Kherbouche <kaminek92@gmail.com>
2023-01-15 15:09:32 +0100

    terraform: spawn 1 instance
    Signed-off-by: Amine Kherbouche <kaminek92@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,55 +1,55 @@
 
-# locals {
-#   fleet_count = 3
-#   flavour     = "2xCPU-4GB"
-#   region      = "uk-lon1"
-# }
+locals {
+  fleet_count = 1
+  flavour     = "2xCPU-4GB"
+  region      = "uk-lon1"
+}
 
-# resource "upcloud_server" "worker" {
-#   count    = local.fleet_count
-#   plan     = local.flavour
-#   hostname = "node${count.index}"
-#   zone     = local.region
+resource "upcloud_server" "worker" {
+  count    = local.fleet_count
+  plan     = local.flavour
+  hostname = "node${count.index}"
+  zone     = local.region
 
-#   template {
-#     storage = "01000000-0000-4000-8000-000030220200"
-#     size    = 80
-#   }
+  template {
+    storage = "01000000-0000-4000-8000-000030220200"
+    size    = 80
+  }
 
-#   network_interface {
-#     type = "public"
-#   }
+  network_interface {
+    type = "public"
+  }
 
-#   network_interface {
-#     type                = "private"
-#     bootable            = false
-#     source_ip_filtering = false
-#     ip_address_family   = "IPv4"
-#     network             = upcloud_network.k8s_vpc.id
-#     ip_address          = "10.8.0.${format("%d", count.index + 100)}"
-#   }
+  network_interface {
+    type                = "private"
+    bootable            = false
+    source_ip_filtering = false
+    ip_address_family   = "IPv4"
+    network             = upcloud_network.k8s_vpc.id
+    ip_address          = "10.32.0.${format("%d", count.index + 100)}"
+  }
 
-#   labels = {
-#     cluster = "homelab"
-#     env     = "prod"
-#     node    = "worker${count.index}"
-#   }
+  labels = {
+    cluster = "homelab"
+    env     = "prod"
+    node    = "worker${count.index}"
+  }
 
-#   login {
-#     keys = data.github_user.kaminek.ssh_keys
-#   }
+  login {
+    keys = data.github_user.kaminek.ssh_keys
+  }
 
-#   metadata = true
-# }
+  metadata = true
+}
 
-# resource "upcloud_network" "k8s_vpc" {
-#   name = "k8s-vpc"
-#   zone = local.region
+resource "upcloud_network" "k8s_vpc" {
+  name = "k8s-vpc"
+  zone = local.region
 
-#   ip_network {
-#     address            = "10.8.0.0/24"
-#     dhcp               = true
-#     dhcp_default_route = false
-#     family             = "IPv4"
-#   }
-# }
+  ip_network {
+    address            = "10.32.0.0/24"
+    dhcp               = true
+    dhcp_default_route = false
+    family             = "IPv4"
+  }
+}
