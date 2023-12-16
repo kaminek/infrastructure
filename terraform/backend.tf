fb20326f93c84d361d5f0951aba20b889952ce03
Amine Kherbouche <kaminek92@gmail.com>
2023-12-17 00:29:14 +0100

    terraform: remove local backend config
    Signed-off-by: Amine Kherbouche <kaminek92@gmail.com>

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "remote" {
-    organization = "kaminek"
-    workspaces {
-      name = "infrastructure"
-    }
-  }
-}
+# terraform {
+#   backend "remote" {
+#     organization = "kaminek"
+#     workspaces {
+#       name = "infrastructure"
+#     }
+#   }
+# }
